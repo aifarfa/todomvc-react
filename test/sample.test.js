9472785b594e12b76001a67d0137097c8ccc794d
pasit.r <kzneko@gmail.com>
2016-03-24 21:36:51 +0700

    just a sample mocha test

---
+++ test/sample.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+describe('sample unit tests', () => {
+
+  it('should do somethings', () => {
+    expect(0).to.be.false;
+  });
+
+});
