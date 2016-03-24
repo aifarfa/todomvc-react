7a74099a670d723000cf453b13a7404c7da6239f
pasit.r <kzneko@gmail.com>
2016-03-24 22:59:06 +0700

    create some React components

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+var TodoApp = React.createClass({
+
+  render: function(){
+      return (
+        <div>
+          <TodoList />
+          <AppControls />
+        </div>
+      );
+  }
+});
+
+var AppControls = React.createClass({
+  render: function() {
+    return (
+      <div>
+      <h2>AppControls component</h2>
+      <TodoCount />
+      </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    return (
+      <h2>His from the TodoList component</h2>
+    );
+  }
+});
+
+var TodoCount = React.createClass({
+  render: function(){
+    return (
+      <span className="todo-count"><strong>0</strong> item left</span>
+    );
+  }
+});
 
 ReactDOM.render(
-  <h2>Hello, React is here</h2>,
+  <TodoApp />,
   document.getElementById('app')
 );
